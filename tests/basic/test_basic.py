da52f14589da4e59af6243ce0335d0bc250e65c3
atu4403 <73111778+atu4403@users.noreply.github.com>
2022-10-14 16:44:38 +0900

    ✨ feat: pytest.mark.parametrizeを追加

--- tests/basic/test_basic.py
+++ tests/basic/test_basic.py
@@ -9,3 +9,22 @@ class TestErrors:
         assert e.type == ZeroDivisionError
         assert e.typename == "ZeroDivisionError"
         assert str(e.value) == "division by zero"
+
+
+# 引数を変えて行うtest
+def add(a, b):
+    if type(a) is not int or type(b) is not int:
+        raise TypeError
+    return a + b
+
+
+@pytest.mark.parametrize("a,b,expected", [(1, 2, 3), (4, 5, 9), (10, 23, 33)])
+def test_add(a, b, expected):
+    assert add(a, b) == expected
+
+
+# 引数を変えて、いずれも例外を吐くことを確認するtest
+@pytest.mark.parametrize("a,b,expected", [("1", 2, 3), (None, 5, 9), (10, [1], 33)])
+def test_add_2(a, b, expected):
+    with pytest.raises(TypeError):
+        add(a, b)
